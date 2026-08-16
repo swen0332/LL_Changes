994e32204244c11bcf9ba25005fc8fe3f6deaf4b
LubeLogger Custom <lubelogger@local.dev>
2026-08-15 21:46:00 -0500

    feat(pump-ui): auto-launch pump on vehicle select, add FAB and sticky record nav

--- wwwroot/js/pump-nav.js
+++ wwwroot/js/pump-nav.js
@@ -5,15 +5,17 @@
 (function () {
     'use strict';
 
-    // Override viewVehicle so selecting a vehicle always opens the Gas tab
+    // Override viewVehicle so selecting a vehicle from Garage opens Gas tab with auto-pump flag
     window.viewVehicle = function (vehicleId, tab) {
-        window.location.href = '/Vehicle/Index?vehicleId=' + vehicleId + '&tab=gas';
+        window.location.href = '/Vehicle/Index?vehicleId=' + vehicleId + '&tab=gas&pump=1';
     };
 
     function patchGarage() {
         if (typeof handleGarageItemClick === 'function') {
             window.handleGarageItemClick = function (e, vehicleId) {
-                window.location.href = '/Vehicle/Index?vehicleId=' + vehicleId + '&tab=gas';
+                if (!$(e.target).hasClass('btn') && !$(e.target).hasClass('dropdown-item')) {
+                    window.location.href = '/Vehicle/Index?vehicleId=' + vehicleId + '&tab=gas&pump=1';
+                }
             };
         }
     }
